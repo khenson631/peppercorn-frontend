90d23544066957fa28d7a94293444f07db70010b
Kyle Henson <139926730+khenson631@users.noreply.github.com>
2025-06-02 21:21:50 -0400

    frontend deployment to github pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/peppercorn/',
+  base: '/peppercorn-frontend/',
   plugins: [react()],
   server: {
     proxy: {
