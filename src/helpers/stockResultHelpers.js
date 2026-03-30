53ba2d394055dcf74b27ea63e843bcb98ca59012
khenson3 <khenson@multidataservices.com>
2026-03-30 16:46:21 -0400

    add more fields to stock profile tab
    restore package.json
    
    add libphonenumber-js for phone number handling
    
    add import map for libphonenumber-js (esm.sh)

---
+++ src/helpers/stockResultHelpers.js
@@ -0,0 +1,8 @@
+export function formatMarketCap(value) {
+    const num = value * 1_000_000;
+  
+    if (num >= 1e12) return (num / 1e12).toFixed(2) + 'T';
+    if (num >= 1e9) return (num / 1e9).toFixed(2) + 'B';
+    if (num >= 1e6) return (num / 1e6).toFixed(2) + 'M';
+    return num.toLocaleString();
+  }  
